06fa8041ce735ee933b4b90af194a10ef1ea03e1
Jefferson J <130589639+JeffersonCallupe@users.noreply.github.com>
2023-06-21 10:31:23 -0500

    Base de datos
    Sistema de registro de vuelos aéreos

---
+++ Base de datos/db_vuelos.sql
@@ -0,0 +1,112 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bd_vuelos
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pasajeros`
+--
+
+DROP TABLE IF EXISTS `pasajeros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pasajeros` (
+  `id_pasajero` int NOT NULL,
+  `nombres` varchar(45) DEFAULT NULL,
+  `apellidos` varchar(45) DEFAULT NULL,
+  `edad` int DEFAULT NULL,
+  `telefono` varchar(9) DEFAULT NULL,
+  PRIMARY KEY (`id_pasajero`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pasajeros`
+--
+
+LOCK TABLES `pasajeros` WRITE;
+/*!40000 ALTER TABLE `pasajeros` DISABLE KEYS */;
+INSERT INTO `pasajeros` VALUES (212000,'jefferson','callupe',22,'923869298');
+/*!40000 ALTER TABLE `pasajeros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reserva`
+--
+
+DROP TABLE IF EXISTS `reserva`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reserva` (
+  `id_reserva` int NOT NULL AUTO_INCREMENT,
+  `id_pasajero` int DEFAULT NULL,
+  `id_vuelo` int DEFAULT NULL,
+  PRIMARY KEY (`id_reserva`),
+  KEY `id_vuelo_registro de vuelos_reservar vuelos_idx` (`id_vuelo`),
+  KEY `id_pasajeros_pasajero_reserva_idx` (`id_pasajero`),
+  CONSTRAINT `id_pasajeros_pasajeros_reserva` FOREIGN KEY (`id_pasajero`) REFERENCES `pasajeros` (`id_pasajero`) ON UPDATE CASCADE,
+  CONSTRAINT `id_vuelo_vuelos_reserva` FOREIGN KEY (`id_vuelo`) REFERENCES `vuelos` (`id_vuelo`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reserva`
+--
+
+LOCK TABLES `reserva` WRITE;
+/*!40000 ALTER TABLE `reserva` DISABLE KEYS */;
+INSERT INTO `reserva` VALUES (1,212000,1);
+/*!40000 ALTER TABLE `reserva` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vuelos`
+--
+
+DROP TABLE IF EXISTS `vuelos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vuelos` (
+  `id_vuelo` int NOT NULL AUTO_INCREMENT,
+  `destino` varchar(45) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `tipo_clase` varchar(45) DEFAULT NULL,
+  `precio` float DEFAULT NULL,
+  `aforo` int DEFAULT NULL,
+  PRIMARY KEY (`id_vuelo`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vuelos`
+--
+
+LOCK TABLES `vuelos` WRITE;
+/*!40000 ALTER TABLE `vuelos` DISABLE KEYS */;
+INSERT INTO `vuelos` VALUES (1,'Piura','2023-07-13','00:00:05','platinium',100,30),(2,'Lima','2023-11-13','10:00:00','clasico',30,40);
+/*!40000 ALTER TABLE `vuelos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-21 10:29:37
